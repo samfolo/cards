0711aa7c29d9cf5e505615f654a4d49976a72bce
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-21 09:15:04 +0000

    write tests for deal_to and refresh_hand, code to pass

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,2 +1,15 @@
 class Dealer
+  def deck
+    arr = []
+    52.times { arr << 'o' }
+    arr
+  end
+
+  def deal_to(*players)
+    players.each { |player| 5.times { player.hand << 'card' } }
+  end
+
+  def refresh_hand(*players)
+    players.each { |player| (5 - player.hand.length).times { player.hand << 'card' } }
+  end
 end
--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -1,4 +1,21 @@
 require 'dealer'
 
 RSpec.describe Dealer do
+  let(:player_1) { double(:player, hand: []) }
+  let(:player_2) { double(:player, hand: []) }
+  let(:short_hand_player) { double(:player, hand: ['card', 'card', 'card']) }
+  let(:test_dealer) { Dealer.new }
+
+  it 'deals cards' do
+    test_dealer.deal_to(player_1, player_2)
+
+    expect(player_1.hand.length).to be 5
+    expect(player_2.hand.length).to be 5
+  end
+
+  it 'refreshes a hand when given cards' do
+    test_dealer.refresh_hand(player_1)
+
+    expect(player_1.hand.length).to be 5
+  end
 end
